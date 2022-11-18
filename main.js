54db6bc778d07ceabf76593da336a53dd603d98c
Dpscoderhs <104405006+Dpscoderhs@users.noreply.github.com>
2022-11-18 20:10:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,9 +13,14 @@ function gotResults(error, results) {
 
     } else {
         console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
 
         document.getElementById("result_label").innerHTML = 'I can here - ' + results[0].label;
         document.getElementById("result_confidence").innerHTML = 'Detected Voice Of - ' + (results[0].confidence * 100).toFixed(2) + " %";
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + ", " + random_number_g + "," + random_number_b + ")";
+        document.getElementById("result_confidence").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
 
 
         img = document.getElementById('photo-1611915387288-fd8d2f5f928b.jpeg')
